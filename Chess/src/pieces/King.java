303e6b4df155ded9e270883da5468d5faf28b567
OberstK <Benjamin.Kurczyk@gmail.com>
2012-07-06 12:16:07 +0200

    King added

---
+++ Chess/src/pieces/King.java
@@ -0,0 +1,5 @@
+package pieces;
+
+public class King extends Piece{
+
+}
--- Chess/src/pieces/Piece.java
+++ Chess/src/pieces/Piece.java
@@ -1,5 +1,29 @@
 package pieces;
 
 public class Piece {
-
+	
+	private String owner;
+	private int positionX;
+	private int positionY;
+	
+	
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public int getPositionX() {
+		return positionX;
+	}
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+	public int getPositionY() {
+		return positionY;
+	}
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+	
 }
